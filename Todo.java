d964bfa1c8a693c728e3dcb2cdb055b8b199db6a
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-02-16 22:15:46 -0600

    Todo filled out

--- Todo.java
+++ Todo.java
@@ -1,18 +1,27 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 11
+Last Modified_______: 2019-02-16
+OU ID_______________: 113334522
+OU EMAIL____________: eid.s.zaben-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Complete the calculateTwoTokens Method
+2. Complete the calculateThreeTokens Method
+3. Complete the execute method
+4. Complete the parse and execute method
+5. Organize all the tests in the CalculatorTest class to test properly
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 15
+2. 15
+3. 25
+4. 30
+5. 70
 
 Actual Time (minutes):
-1. XXX
-2. XXX
-*/}
+1. 20
+2. 15
+3. 25
+4. 40
+5. 60
+*/}S
